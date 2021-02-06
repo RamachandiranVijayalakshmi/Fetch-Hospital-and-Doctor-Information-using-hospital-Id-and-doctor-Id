26e0c191a337998e27bdcc31e63aa7917a8a3363
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2021-02-06 23:55:09 +0530

    Add files via upload

---
+++ Doctor data.py
@@ -0,0 +1,8 @@
+def get_hospital_detail(hospital_id):
+    #Read data from Hospital table
+
+def get_doctor_detail(doctor_id):
+    # Read data from Doctor table
+
+get_hospital_details(2)
+get_doctor_details(105)
